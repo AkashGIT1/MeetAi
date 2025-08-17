bf4def916b15749b92305cfc613eae1e544d7c1f
Akash <akashkvs91@gmail.com>
2025-08-18 05:21:47 +0530

    Created  UI For Sign In Page of our Milan.AI - downloaded and used logo from logoipsum - Implemented its error functionalities using useState hooks. - Used shadcn ui components to build form and styles them with tailwind css. - Used zod resolver too to validate.

---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,8 @@
+import { SignUnView } from "@/app/modules/auth/ui/views/sign-up-view";
+
+const Page=()=>{
+    return(
+        <SignUnView />
+    )
+}
+export default Page;
---
+++ src/app/modules/auth/ui/views/sign-up-view.tsx
@@ -0,0 +1,12 @@
+"use client"
+import { Card } from '@/components/ui/card';
+
+export const SignUnView = () => {
+  return (
+    <Card>
+      <h1>Sign up ka form </h1>
+      {/* Sign-in form goes here */}
+    </Card>
+  );
+
+};
