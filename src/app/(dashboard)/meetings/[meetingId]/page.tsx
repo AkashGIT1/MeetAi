a585e62b3e639d68bace1f4e46d6e7dd1a53cef7
Akash <akashkvs91@gmail.com>
2025-08-26 03:59:01 +0530

    16- Meetings Setup -Added Meetings Schema -Added Meetings module -Added Meetings procedure -Added Meeting Pages

---
+++ src/app/(dashboard)/meetings/[meetingId]/page.tsx
@@ -0,0 +1,10 @@
+
+const Page = () => {
+  return (
+    <div>
+      <h1>Meeting ID Page</h1>
+    </div>
+  );
+};
+
+export default Page;
---
+++ src/modules/meetings/server/procedures.ts
@@ -0,0 +1,103 @@
+import { z } from "zod";
+import { db } from "@/db";
+import {meetings } from "@/db/schema";
+import {
+  createTRPCRouter,
+  protectedProcedure,
+} from "@/trpc/init";
+import { and, count, desc, eq, getTableColumns, ilike } from "drizzle-orm";
+import { TRPCError } from "@trpc/server";
+import { DEFAULT_PAGE, DEFAULT_PAGE_SIZE, MAX_PAGE_SIZE, MIN_PAGE_SIZE } from "@/constants";
+
+export const meetingsRouter = createTRPCRouter({
+  getOne: protectedProcedure
+    .input(z.object({ id: z.string() }))
+    .query(async ({ input, ctx }) => {
+      try {
+        const [existingMeeting] = await db
+          .select({
+            ...getTableColumns(meetings),
+       
+          })
+          .from(meetings)
+          .where(and(
+            eq(meetings.id, input.id),
+            eq(meetings.userId, ctx.auth.user.id),
+          )
+        );
+
+        if (!existingMeeting) {
+          throw new TRPCError({
+            code: 'NOT_FOUND',
+            message: 'Meeting not found'
+          });
+        }
+
+        return existingMeeting;
+      } catch (error) {
+        throw new TRPCError({
+          code: 'INTERNAL_SERVER_ERROR',
+          message: 'Failed to fetch meeting',
+          cause: error
+        });
+      }
+    }),
+
+  getMany: protectedProcedure
+    .input(z.object({
+      page: z.number().default(DEFAULT_PAGE),
+      pageSize: z
+      .number()
+      .min(MIN_PAGE_SIZE)
+      .max(MAX_PAGE_SIZE)
+      .default(DEFAULT_PAGE_SIZE),
+      search: z.string().nullish(),
+    })
+  )
+    .query(async ({ ctx, input }) => {
+
+      const {search, page, pageSize} = input; 
+
+      try {
+        const data = await db
+          .select({
+             ...getTableColumns(meetings),
+          })
+          .from(meetings)
+          .where(
+            and(
+              eq(meetings.userId, ctx.auth.user.id),
+              search ? ilike(meetings.name, `%${search}%`) : undefined,
+            )
+          )
+          .orderBy(desc(meetings.createdAt), desc(meetings.id))
+          .limit(pageSize)
+          .offset((page - 1) * pageSize)
+
+        const total = await db
+          .select({
+            count: count(),
+          })
+          .from(meetings)
+          .where(
+            and(
+              eq(meetings.userId, ctx.auth.user.id),
+              search ? ilike(meetings.name, `%${search}%`) : undefined,
+            )
+          );
+          
+        const totalPages = Math.ceil(total[0].count / pageSize);
+        return {
+          items: data,
+          total: total[0].count,
+          totalPages,
+         };
+      } catch (error) {
+        throw new TRPCError({
+          code: 'INTERNAL_SERVER_ERROR',
+          message: 'Failed to fetch agents',
+          cause: error
+        });
+      }
+    }),
+});
