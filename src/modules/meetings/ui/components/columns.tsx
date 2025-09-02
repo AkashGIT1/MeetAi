b9683963e315e063d11d4c598272ca0f05127edb
Akash <akashkvs91@gmail.com>
2025-09-02 16:57:18 +0530

    25-Completed States Added Summary Tab Added Recording tab

--- src/modules/meetings/ui/components/columns.tsx
+++ src/modules/meetings/ui/components/columns.tsx
@@ -1,7 +1,6 @@
 "use client"
 
 import { format} from "date-fns";
-import humanizeDuration from "humanize-duration";
 
 import { ColumnDef } from "@tanstack/react-table"
 import { MeetingGetMany } from "../../types"
@@ -16,7 +15,7 @@ import {
 
 } from "lucide-react"
 
-import { cn } from "@/lib/utils"
+import { cn, formatDuration } from "@/lib/utils"
 
 import { Badge } from "@/components/ui/badge"
 
@@ -24,13 +23,6 @@ import { Badge } from "@/components/ui/badge"
 // This type is used to define the shape of our data.
 // You can use a Zod schema here if you want.
 
-function formatDuration(seconds: number) {
-  return humanizeDuration(seconds * 1000, {
-    largest: 1,
-    round: true,
-    units: ["h", "m", "s"],
-  });
-};
 
 const statusIconMap = {
   upcoming: ClockArrowUpIcon,
